22b7cd5f03f73cadf7a7783d1f402bae551e5b1c
Vladimir Volkov <volkov_vd@drom.ru>
2021-03-31 13:30:28 +1000

    [ADMIN] Добавил функцию удаления качественного значения в редактор возможных значений

--- public/assets/js/possibleValue.js
+++ public/assets/js/possibleValue.js
@@ -1,9 +1,24 @@
+let del = function (event) {
+    let target = event.target;
+    target.nextElementSibling.remove();
+    target.previousElementSibling.remove();
+    target.remove();
+}
+
 document.getElementById('add').addEventListener('click', function (event) {
     let input = document.createElement('input');
+    let delButton = document.createElement('input');
+    delButton.type = 'button';
+    delButton.value = '-';
+    delButton.addEventListener('click', del);
     input.type = 'text';
     input.className = 'value';
     input.name = 'values[]';
-    let pos = document.getElementsByClassName('value').length;
-    document.getElementsByClassName('value').item(pos-1).after(document.createElement('br'), input);
+    input.required = true;
+    document.getElementById('add').before(input, delButton, document.createElement('br'));
 });
 
+let delItem  = document.getElementsByClassName('del');
+Array.from(delItem).forEach(function (item) {
+    item.addEventListener('click', del);
+});
