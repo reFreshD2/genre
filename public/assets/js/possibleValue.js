afce66dfd0b9f8abf34e6c78bba3a730d7178e48
refreshjss <volkov_vd@drom.ru>
2021-04-04 16:06:50 +1000

    [STYLE] Добавил стили интерфейсов для индексов и кастомных форм

--- public/assets/js/possibleValue.js
+++ public/assets/js/possibleValue.js
@@ -1,22 +1,40 @@
 let del = function (event) {
     let target = event.target;
-    target.nextElementSibling.remove();
-    target.previousElementSibling.remove();
+    target.parentNode.remove();
     target.remove();
 }
 
 document.getElementById('add').addEventListener('click', function (event) {
-    let input = document.createElement('input');
+    document.getElementById('add').parentNode.before(createGroup());
+});
+
+function createGroup() {
+    let divGroup = document.createElement('div');
+    divGroup.className = 'group';
+    let label = document.createElement('label');
+    label.className = 'dis';
+    label.append('Значение качественного признака ');
     let delButton = document.createElement('input');
     delButton.type = 'button';
-    delButton.value = '-';
+    delButton.value = 'x';
+    delButton.className = 'del';
     delButton.addEventListener('click', del);
+    let input = document.createElement('input');
     input.type = 'text';
-    input.className = 'value';
+    input.className = 'value input';
     input.name = 'values[]';
     input.required = true;
-    document.getElementById('add').before(input, delButton, document.createElement('br'));
-});
+    let highlight = document.createElement('span');
+    highlight.className = 'highlight';
+    let bar = document.createElement('span');
+    bar.className = 'bar';
+    divGroup.appendChild(label);
+    divGroup.appendChild(delButton);
+    divGroup.appendChild(input);
+    divGroup.appendChild(highlight);
+    divGroup.appendChild(bar);
+    return divGroup;
+}
 
 let delItem  = document.getElementsByClassName('del');
 Array.from(delItem).forEach(function (item) {
